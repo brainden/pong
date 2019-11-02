99f897dc9af67670b8b2b1abf2689b11a7da39df
Brandon Kilic <brandonkilic@gmail.com>
2019-11-01 20:48:31 -0400

    added ball to game

--- Pong1.py
+++ Pong1.py
@@ -24,7 +24,17 @@ paddle_b.color("white")
 paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
+
+
 # Ball
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
 
 # Main game loop
 while True:
